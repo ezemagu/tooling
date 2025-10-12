9d99f6dcca660fb745e17f2b6f5d93b9b13aa31d
Emmanuel <ezemagu@yahoo.com>
2025-10-12 23:17:12 +0100

    update our s3

--- modules/s3-dynamodb/variables.tf
+++ modules/s3-dynamodb/variables.tf
@@ -1,7 +1,7 @@
 variable "bucket" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "class55-terraform-backend-bucket"
+  default     = "class56-terraform-backend-bucket"
 }
 
 variable "table" {
@@ -13,6 +13,6 @@ variable "table" {
 variable "region" {
   description = "The AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
