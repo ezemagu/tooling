4de86c8dab4c9b59cf42419972eba598b1480e50
Emmanuel <ezemagu@yahoo.com>
2025-10-12 23:37:40 +0100

    update

--- variables.tf
+++ variables.tf
@@ -93,5 +93,5 @@ variable "dynamodb_table" {
 variable "main-region" {
   description = "The AWS region to deploy resources"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
